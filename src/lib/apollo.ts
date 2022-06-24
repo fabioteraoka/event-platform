360ec517a7248d5fad8fceab16ddc7626e3d6fae
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2022-06-24 12:32:21 -0300

    Aula 4

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,10 @@
 import {ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o1po5y052n01xm2e7j9e17/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers:{
+      'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
   });
 
