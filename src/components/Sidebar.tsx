e214fe3ce538f3e246756556e31267cd35e8e50c
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2022-06-25 08:35:47 -0300

    Aula 5 - final

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,31 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../generated";
 import { Lesson } from "./Lesson";
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-      id
-      lessonType
-      availableAt
-      title
-      slug
-    }
-  }
-`;
-
-interface GetLessonsQueryResponse {
-  lessons: {
-    id: string;
-    title: string;
-    slug: string;
-    availableAt: string;
-    lessonType: "live" | "class";
-  }[];
-}
-
 export function Sidebar() {
-  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
-
+  const { data } = useGetLessonsQuery()
   return (
     <aside className="w-[348px] invisible md:visible bg-gray-700 p-6 border-l border-gray-600">
       <span className="block pb-6 mb-6 text-2xl font-bold border-b border-gray-500">
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,66 +1,34 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
-import { gql, useQuery } from "@apollo/client";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
 
 import '@vime/core/themes/default.css'
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String) {
-    lesson(where: {slug: $slug}) {
-      title
-      videoId
-      description
-      teacher {
-        bio
-        avatarURL
-        name
-      }
-    }
-  }
-`
-
-interface GetLessonBySlugResponse {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string;
-    }
-  }
-}
+import { useGetLessonBySlugQuery } from "../generated";
 
 interface VideoProps {
   lessonSlug: string;
 }
 
-export function Video({lessonSlug}: VideoProps) {
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+export function Video(props: VideoProps) {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
-      slug: lessonSlug,
+      slug: props.lessonSlug,
     },
-    fetchPolicy:"no-cache"
-  })
+  });
 
-  if (!data) {
+  if (!data || !data.lesson) {
     return (
       <div className="flex-1">
         <p>Carregando...</p>
       </div>
-    )
+    );
   }
-
-  const { lesson } = data;
-  const {videoId, title, teacher, description } = lesson
 
   return (
     <div className="flex-1">
       <div className="flex justify-center bg-black">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
-          <Player>
-            <Youtube videoId={videoId} />
+        <Player>
+            <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -69,34 +37,44 @@ export function Video({lessonSlug}: VideoProps) {
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="items-start block gap-16 md:flex">
           <div className="flex-1 mb-4">
-            <h1 className="text-2xl font-bold">
-              {title}
-            </h1>
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
             <p className="mt-4 leading-relaxed text-gray-200">
-              {description}
+              {data.lesson.description}
             </p>
 
-            <div className="flex items-center gap-4 mt-6">
-              <img
-                className="w-16 h-16 border-2 border-blue-500 rounded-full"
-                src={teacher.avatarURL}
-                alt=""
-              />
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  className="w-16 h-16 border-2 border-blue-500 rounded-full"
+                  src={data.lesson.teacher.avatarURL}
+                  alt=""
+                />
 
-              <div className="leading-relaxed">
-                <strong className="block text-2xl font-bold">{teacher.name}</strong>
-                <span className="block text-sm text-gray-200">{teacher.bio}</span>
+                <div className="leading-relaxed">
+                  <strong className="block text-2xl font-bold">
+                    {data.lesson.teacher.name}
+                  </strong>
+                  <span className="block text-sm text-gray-200">
+                    {data.lesson.teacher.bio}
+                  </span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
 
           <div className="flex flex-col gap-4 p-16">
-            <a href="" className="flex items-center justify-center gap-2 p-4 text-sm font-bold uppercase transition-colors bg-green-500 rounded hover:bg-green-700">
+            <a
+              href=""
+              className="flex items-center justify-center gap-2 p-4 text-sm font-bold uppercase transition-colors bg-green-500 rounded hover:bg-green-700"
+            >
               <DiscordLogo size={24} />
               Comunidade do Discord
             </a>
 
-            <a href="" className="flex items-center justify-center gap-2 p-4 text-sm font-bold text-blue-500 uppercase transition-colors border border-blue-500 rounded hover:bg-blue-500 hover:text-gray-900">
+            <a
+              href=""
+              className="flex items-center justify-center gap-2 p-4 text-sm font-bold text-blue-500 uppercase transition-colors border border-blue-500 rounded hover:bg-blue-500 hover:text-gray-900"
+            >
               <Lightning size={24} />
               Acesse o desafio
             </a>
@@ -104,14 +82,18 @@ export function Video({lessonSlug}: VideoProps) {
         </div>
 
         <div className="grid gap-8 mt-20 grid-colos-1 md:grid-cols-2">
-          <a href="" className="flex items-stretch gap-6 overflow-hidden transition-colors bg-gray-700 rounded hover:bg-gray-600">
+          <a
+            href=""
+            className="flex items-stretch gap-6 overflow-hidden transition-colors bg-gray-700 rounded hover:bg-gray-600"
+          >
             <div className="flex items-center h-full p-6 bg-green-700">
               <FileArrowDown size={24} />
             </div>
             <div className="py-6 leading-relaxed">
               <strong className="text-2xl">Material complementar</strong>
               <p className="mt-2 text-sm text-gray-200">
-                Acesse o material complementar para acelerar o seu desenvolvimento
+                Acesse o material complementar para acelerar o seu
+                desenvolvimento
               </p>
             </div>
             <div className="flex items-center h-full p-6">
@@ -119,14 +101,18 @@ export function Video({lessonSlug}: VideoProps) {
             </div>
           </a>
 
-          <a href="" className="flex items-stretch gap-6 overflow-hidden transition-colors bg-gray-700 rounded hover:bg-gray-600">
+          <a
+            href=""
+            className="flex items-stretch gap-6 overflow-hidden transition-colors bg-gray-700 rounded hover:bg-gray-600"
+          >
             <div className="flex items-center h-full p-6 bg-green-700">
               <FileArrowDown size={24} />
             </div>
             <div className="py-6 leading-relaxed">
               <strong className="text-2xl">Wallpapers exclusivos</strong>
               <p className="mt-2 text-sm text-gray-200">
-                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
+                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua
+                máquina
               </p>
             </div>
             <div className="flex items-center h-full p-6">
