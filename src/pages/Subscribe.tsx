d1b6ac691fbb164e46d94378c11c01716d38e730
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2022-06-25 09:58:22 -0300

    Update Image

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,6 +2,7 @@ import {useNavigate} from "react-router-dom";
 import { useState, FormEvent } from "react";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../generated";
+import mockup from "/src/assets/code-mockup.png"
 
 export function Subscribe() {
   const navigate = useNavigate();
@@ -70,7 +71,7 @@ export function Subscribe() {
         </div>
       </div>
 
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+      <img src={mockup} className="mt-10" alt="" />
     </div>
   );
 }
